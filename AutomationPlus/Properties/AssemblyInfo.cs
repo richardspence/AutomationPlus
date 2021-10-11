d3f1b5858b3d308a84597fb80e186ac90af18f9f
Richard Spence <rsjspence@hotmail.com>
2021-10-10 18:16:38 -0700

    Fix build

--- AutomationPlus/Properties/AssemblyInfo.cs
+++ AutomationPlus/Properties/AssemblyInfo.cs
@@ -30,5 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+// [bump]
 [assembly: AssemblyVersion("2021.6.29.0")]
+// [bump]
 [assembly: AssemblyFileVersion("2021.6.29.0")]
